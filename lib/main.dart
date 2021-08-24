e8d4f48c43010afd2e2957c675f2374f9187c931
Vinicius Almeida <vinicius.sec21@gmail.com>
2021-08-24 16:02:55 -0300

    commit app

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+Uri request = Uri.parse('https://api.hgbrasil.com/finance?key=fde32ea9');
+
+void main() async {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('\$ Conversor \$'),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text(
+                    "Carregando dados...",
+                    style: TextStyle(color: Colors.amber, fontSize: 25),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                      child: Text(
+                    "Erro ao carregar dados :(",
+                    style: TextStyle(color: Colors.amber, fontSize: 25),
+                    textAlign: TextAlign.center,
+                  ));
+                } else {
+                  return Container(
+                    color: Colors.green,
+                  );
+                }
+            }
+          }),
+    );
+  }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
